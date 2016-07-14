1c68ea07598edf511d2155d4f753210b287a8378
Methew Guda <methewguda@gmail.com>
2016-07-14 00:05:00 -0700

    Check image file

--- routes/users.js
+++ routes/users.js
@@ -27,23 +27,24 @@ router.post('users/register', function(req, res, next){
     var username = req.body.username;
     var password = req.body.password;
     var password_repeat = req.body.passpassword_repeat;
+
+    //Check for image filed
+    if(req.file.image){
+        //logging image uploading
+        console.log('Uploading Image File..........');
+
+        //File information
+        var image_original      = req.file.image.originalname;
+        var image_profile       = req.file.image.name;
+        var image_profile_MIME  = req.file.image.mimetype;
+        var image_profile_PATH  = req.file.image.path;
+        var image_profile_EXT   = req.file.image.extension;
+        var image_profile_SIZE  = req.file.image.size;
+    }else{
+        //Set a default image
+        var image_profile = 'default_image.png';
+    }
+
 });
-
-//Check for image filed
-if(req.file.image){
-    //logging image uploading
-    console.log('Uploading Image File..........');
-
-    //File information
-    var image_original      = req.file.image.originalname;
-    var image_profile       = req.file.image.name;
-    var image_profile_MIME  = req.file.image.mimetype;
-    var image_profile_PATH  = req.file.image.path;
-    var image_profile_EXT   = req.file.image.extension;
-    var image_profile_SIZE  = req.file.image.size;
-}else{
-    //Set a default image
-    var image_profile = 'default_image.png';
-}
 
 module.exports = router;
